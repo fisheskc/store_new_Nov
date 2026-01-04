78f427982867a3418d42681f7ed5033ddc33751b
Karen Collins <fisheskc@googlemail.com>
2026-01-04 15:02:42 +0000

    Updated the favourites page in the app

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,10 +1,16 @@
+//Your page is being treated as a static page, but it needs to be dynamic.
+// Next.js  tries to statically generate it → your user-dependent function explodes → build fails.
+// The fix: force the page to be dynamic
+// This tells Next.js: “Do NOT try to pre-render this page at build time. Render it on every request.”
+
+export const dynamic = 'force-dynamic';
+
 import SectionTitle from '@/components/global/SectionTitle';
 import ProductsGrid from '@/components/products/ProductsGrid';
 import {  fetchUserFavorites } from '@/utils/actions';
 
 async function FavoritesPage() {
   const favorites = await fetchUserFavorites()
-  // We are not going to provide any value
   // Since product is sitting in the producct property, we want to use map & use the product properties
   if(favorites.length ===0){
     return <SectionTitle text='You have no favourites yet' />
