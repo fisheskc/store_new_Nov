4b29e7b5aaa0dcd8fa9bb1e88d6a10a006064a43
Karen Collins <fisheskc@googlemail.com>
2026-02-01 19:46:11 +0000

    Updated prisma.config.ts file

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
     seed: 'tsx prisma/seed.ts'
-  }
+  },
   datasource: {
    url: env("DATABASE_URL")
   //  url: process.env["DATABASE_URL"],
